94d539f710a21875122d1371401cae3ed08f9bc0
angelsol299 <angelsol299@hotmail.com>
2019-03-16 19:40:04 +0100

    files and basics

---
+++ files/js.js
@@ -0,0 +1,8 @@
+function outputName() {
+  var x, name, a, b, answer;
+  x = document.getElementById("form1");
+  y = x.elements["name"].value;
+
+  document.write("hello" + y + "<br />");
+  document.write(y + "you are back");
+}
