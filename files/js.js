cb272e446b43822b70c50323edbb1a758885ce83
angelsol299 <angelsol299@hotmail.com>
2019-03-17 18:36:22 +0100

    more fixes

--- files/js.js
+++ files/js.js
@@ -45,49 +45,8 @@ const outputName = () => {
   }
   localStorage.setItem("name", JSON.stringify(list));
 
-  /* 
-  console.log(value);
-  let itemsArray = localStorage.getItem("name")
-    ? JSON.parse(localStorage.getItem("name"))
-    : [];
-
-  //if (value) {
-
-  itemsArray.push(value);
-  localStorage.setItem("name", JSON.stringify(itemsArray));
-
-  document.getElementById("demo").innerHTML = "Welcome " + y;
-  */
-  // }
 };
 
-/*
-const welcom = () => {
-  const input2 = document.getElementById("input2");
-  const value = input2.value;
-
-  if (!localStorage.getItem("name")) {
-    localStorage.setItem("name", "[]");
-  }
-
-  const list = JSON.parse(localStorage.getItem("name"));
-  let exist = false;
-  for (var i = 0; i < list.length; i++)
-    if (list[i] == value) {
-      exist = true;
-      break;
-    }
-  if (!exist) {
-    list.push(value);
-    alert("created");
-  } else {
-    alert("exist");
-  }
-  localStorage.setItem("name", JSON.stringify(list));
-};
-
-//hide button when clicked
-//this.style.visibility='hidden';[welcom()]
 
 /*
 window.onload = () => {
@@ -106,33 +65,3 @@ const setColorCookie = () => {
 
   document.getElementById("p1").value = customObject.name;
 };
-
-// local storage
-
-/*
-
-localStorage.setItem("text", "lets do that");
-
-let val = localStorage.getItem("text");
-console.log(val);
-
-let key = "vamos_ya";
-localStorage.setItem(key, 239);
-
-let options = {
-  name: "natoion",
-  profession: "teacher",
-  sports: ["soccer", "basket", "baseball"]
-};
-
-let str = JSON.stringify(options);
-
-localStorage.setItem("arr2", str);
-
-let original = localStorage.getItem("arr2");
-
-console.log(original);
-let obj = JSON.parse(original);
-console.log(obj);
-
-*/
